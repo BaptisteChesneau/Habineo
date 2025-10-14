e7c1008496a5edbb9c6dbefb546b632f22cf53c6
Baptiste Chesneau <baptiste.chesneau@example.com>
2025-10-14 10:25:22 +0200

    first change styles

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Page d'accueil par défaut
 @app.route('/')
-def home():
-    return render_template('hello.html')
-
-@app.route('/accueil')
 def accueil():
     return render_template('accueil.html')
+
+# Page Hello (optionnelle)
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
